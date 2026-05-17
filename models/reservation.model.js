af956325335dff648355502d71878440128fd93a
Emma Heikkinen <emhe2504@student.miun.se>
2026-05-17 19:52:13 +0200

    Uppdaterade informationen som behövs i en bokning

--- models/reservation.model.js
+++ models/reservation.model.js
@@ -6,12 +6,23 @@ const reservationSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "Ange email"],
-        lowercase: true,
-        unique: true
+        lowercase: true
     },
     phonenumber: {
         type: String,
         required: [true, "Ange telefonnummer"]
+    },
+    date: {
+        type: Date,
+        required: [true, "Ange datum"]
+    },
+    time: {
+        type: String,
+        required: [true, "Ange tid"]
+    },
+    people: {
+        type: Number,
+        required: [true, "Ange antal personer"]
     },
     comment: {
         type: String
